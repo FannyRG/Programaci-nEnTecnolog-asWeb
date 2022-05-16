630449281a3afdb9a787845977f8532ab1f1ba0b
Estefania <95757186+FannyRG@users.noreply.github.com>
2022-05-16 00:17:07 -0500

    Add files via upload

---
+++ reactApp/build/precache-manifest.48fe407920c1a960c5f540079227b76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f784d764f4eed01629d0d67374f4b723",
+    "url": "/FannyRG/Programaci-nEnTecnolog-asWeb/index.html"
+  },
+  {
+    "revision": "2188763cff721fd5b149",
+    "url": "/FannyRG/Programaci-nEnTecnolog-asWeb/static/css/main.8025713d.chunk.css"
+  },
+  {
+    "revision": "d678bdae10ef5f9a8711",
+    "url": "/FannyRG/Programaci-nEnTecnolog-asWeb/static/js/2.8e785f5a.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/FannyRG/Programaci-nEnTecnolog-asWeb/static/js/2.8e785f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2188763cff721fd5b149",
+    "url": "/FannyRG/Programaci-nEnTecnolog-asWeb/static/js/main.e2da947d.chunk.js"
+  },
+  {
+    "revision": "89d5d73340abce4d6a01",
+    "url": "/FannyRG/Programaci-nEnTecnolog-asWeb/static/js/runtime-main.8ede590b.js"
+  }
+]);
